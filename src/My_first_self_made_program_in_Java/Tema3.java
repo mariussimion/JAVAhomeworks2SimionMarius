93c96af2d89e0ef17187b103ed00cce98acba704
Marius <Marius@DESKTOP-FHUG1DA>
2017-02-17 17:13:21 +0200

    Commit

---
+++ src/My_first_self_made_program_in_Java/Tema3.java
@@ -0,0 +1,10 @@
+package My_first_self_made_program_in_Java;
+
+import java.util.Scanner;
+public class Tema3 {
+    public static void main (String args[]){;
+    Scanner marius = new Scanner (System.in);
+    System.out.println (marius.nextLine());
+    
+}
+}
