e593faf3d779b2b3be0ef9e5bdde8ffbdc26b352
Marius <Marius@DESKTOP-FHUG1DA>
2017-03-28 14:10:19 +0300

    new Commit

---
+++ test/My_first_self_made_program_in_Java/Tema3IT.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package My_first_self_made_program_in_Java;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Marius
+ */
+public class Tema3IT {
+    
+    public Tema3IT() {
+    }
+
+    /**
+     * Test of main method, of class Tema3.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Tema3.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
