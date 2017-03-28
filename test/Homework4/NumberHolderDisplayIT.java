c59c3815aba8e08855d334753ac7e08cd3cad98c
Marius <Marius@DESKTOP-FHUG1DA>
2017-03-28 14:09:55 +0300

    new Commit

---
+++ test/Homework4/NumberHolderDisplayIT.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Homework4;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Marius
+ */
+public class NumberHolderDisplayIT {
+    
+    public NumberHolderDisplayIT() {
+    }
+
+    /**
+     * Test of main method, of class NumberHolderDisplay.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        NumberHolderDisplay.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
